00a87ac8e3c747771351dbd03888fbbc3f5a90af
julian-bieringer <bieringer.julian@dodax.com>
2013-09-13 23:42:12 +0200

    Added new Class for the checkbox and the FileName

---
+++ DateToDate_Progress/src/dateToDate_Progress/main/FileCheckbox.java
@@ -0,0 +1,22 @@
+package dateToDate_Progress.main;
+
+import javax.swing.JCheckBox;
+
+public class FileCheckbox {
+
+	public String getFileName() {
+		return fileName;
+	}
+	public void setFileName(String fileName) {
+		this.fileName = fileName;
+	}
+	public JCheckBox getCheckBox() {
+		return checkBox;
+	}
+	public void setCheckBox(JCheckBox checkBox) {
+		this.checkBox = checkBox;
+	}
+	private String fileName;
+	private JCheckBox checkBox;
+	
+}
